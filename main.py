84bd5367ae6491ccb80ff54a36767789df3a1808
FatimaAliakberli <149687920+FatimaAliakberli@users.noreply.github.com>
2024-06-05 12:04:45 +0400

    Update main.py
    configuration of user page is updated

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+def main():
+  st.set_page_config(
+    page_title = "Personal Chatbot"
+    page_icon = "💬"
+  )
+
+  st.header("Welcome to your Personal Chatbot 🤖")
+
+  with st.sidebar:
+    user_input = st.text_input("Your message: ", key = "user_input")
+  
+
+
+if __name__ == "__main__":
+  main()
