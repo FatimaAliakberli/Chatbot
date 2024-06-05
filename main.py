024b8402b3d497f622c8bee9b2512f1ef496e137
FatimaAliakberli <149687920+FatimaAliakberli@users.noreply.github.com>
2024-06-05 12:41:45 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-#from streamlit_chat import message
 
 def main():
   st.set_page_config(
@@ -9,8 +8,7 @@ def main():
 
   st.header("Welcome to your Personal Chatbot 🤖")
 
-  #message("Hello, how are you?")
-  #message("I am good", is_user = True)
+ 
 
   with st.sidebar:
     user_input = st.text_input("Your message: ", key = "user_input")
